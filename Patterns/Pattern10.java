8702e2e97912d39917b8eccba7d20b0e2273e0f6
sowmika03 <vemulasaisowmika@gmail.com>
2025-06-18 12:43:34 +0530

    Create Pattern10.java

---
+++ Patterns/Pattern10.java
@@ -0,0 +1,17 @@
+class Pattern10
+{
+public static void main(String[]args)
+{
+String str="INDIA";
+for(int i=1;i<=str.length();i++)
+{
+System.out.println(str.substring(0,i));
+}
+}
+}
+/*
+I
+IN
+IND
+INDI
+INDIA*/
