ba5bd3b35fb8ff4f3c7284415a3d435b3d7786ea
sowmika03 <vemulasaisowmika@gmail.com>
2025-06-18 12:46:49 +0530

    Update Pattern2.java

--- Patterns/Pattern2.java
+++ Patterns/Pattern2.java
@@ -6,9 +6,16 @@ class Pattern2
 	{
 	    for(int j=1;j<6;j++)
 	{
-	    System.out.print(i);
+	    System.out.print(i +" ");
 	}
 	System.out.println();
 	}
     }
 }
+/*
+1 1 1 1 1
+2 2 2 2 2
+3 3 3 3 3
+4 4 4 4 4
+5 5 5 5 5
+*/
