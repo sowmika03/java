2736db5be37b7b1426d07a9ad1fe7b5f4bcd1716
sowmika03 <vemulasaisowmika@gmail.com>
2025-06-20 14:35:18 +0530

    Add files via upload

---
+++ Patterns/Pattern11.java
@@ -0,0 +1,23 @@
+/* Pyramid 1,3,5,7,9*/
+class Pattern11 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+    *
+   ***
+  *****
+ *******
+*********
+
+*/
---
+++ Patterns/Pattern14.java
@@ -0,0 +1,24 @@
+/* hallow square 5 */
+public class Pattern14 {
+    public static void main(String[] args) {
+        int rows = 5;
+        int cols = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (i == 1 || i == rows || j == 1 || j == cols) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+/* 
+* * * * * 
+*       * 
+*       * 
+*       * 
+* * * * * 
+*/
