079dcd8c809443c48b748e5d4dd2f0ed2455ec85
sowmika03 <vemulasaisowmika@gmail.com>
2025-06-21 16:52:01 +0530

    Upload Split.java

---
+++ Strings/Split.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+class Split{
+  public static void main(String[] args) {
+    String s1="vemula sai sowmika";
+    String[] s2=s1.split(" ");
+    System.out.println(Arrays.toString(s2));
+  }
+}
+/*
+[vemula, sai, sowmika]
+*/
